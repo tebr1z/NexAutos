4a9cee8fa17abbf312e0ca94ec23cb40b15dc0ac
tebr1z <tabrizyh@code.edu.az>
2026-09-04 14:17:15 +0400

    Feat Search

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import { config } from "dotenv";
+
+config({ path: ".env" });
 
 const prisma = new PrismaClient();
 
